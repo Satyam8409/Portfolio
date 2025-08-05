8115e944a34b3e2c703ae14e868ba197b13c8b93
Satyam <singhsatyam8409@gmail.com>
2025-08-05 19:15:56 +0530

    bug

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -3,7 +3,7 @@ import { education } from "../../constants"; // Import the education data
 const Education=()=>{
   return (
     <section id="education" className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-3">
-      {/* Section Title */}
+      {/*Section Title */}
       <div className="text-center mb-16">
         <h2 className="text-4xl font-bold text-white">EDUCATION</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
@@ -12,30 +12,28 @@ const Education=()=>{
         </p>
       </div>
 
-      {/* Education Timeline */}
+      {/*Education*/}
       <div className="relative">
-        {/* Vertical line */}
+        {/*Vertical line*/}
         <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 sm:-translate-x-0 w-1 bg-white h-full"></div>
 
-        {/* Education Entries */}
+        {/*Education*/}
         {education.map((edu, index) => (
           <div key={edu.id} className={`flex flex-col sm:flex-row items-center mb-16 ${index % 2 === 0 ? "sm:justify-start" : "sm:justify-end"}`}>
-            {/* Content Section */}
+            {/*Content Section*/}
             <div className={`w-full sm:max-w-md p-4 sm:p-8 rounded-2xl shadow-2xl border border-white bg-gray-900 backdrop-blur-md shadow-[0_0_20px_1px_rgba(130,69,236,0.3)] ${index % 2 === 0 ? "sm:ml-0" : "sm:mr-0"} sm:ml-44 sm:mr-44 ml-8 transform transition-transform duration-300 hover:scale-105`}>
-              {/* Flex container for image and text */}
               <div className="flex items-center space-x-6">
-                {/* School Logo/Image */}
+                {/*School Logo/Image*/}
                 <div className="w-24 h-24 bg-white rounded-md overflow-hidden">
                   <img src={edu.img} alt={edu.school} className="w-full h-full object-cover"/>
                 </div>
-
-                {/* Degree, School Name, and Date */}
+                {/*Degree*/}
                 <div className="flex flex-col justify-between">
                   <div>
                     <h3 className="text-xl sm:text-xl font-semibold text-white">{edu.degree}</h3>
                     <h4 className="text-md sm:text-sm text-gray-300">{edu.school}</h4>
                   </div>
-                  {/* Date at the bottom */}
+                  {/*Date*/}
                   <p className="text-sm text-gray-500 mt-2">{edu.date}</p>
                 </div>
               </div>
